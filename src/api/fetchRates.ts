f7f4be6e79baefed77eaf9ecb92239e9bd8f0175
MatejB <bouska.matej@gmail.com>
2024-05-31 16:06:43 +0200

    adjusted api call for proxy server

--- src/api/fetchRates.ts
+++ src/api/fetchRates.ts
@@ -1,10 +1,14 @@
 /** 
- * PROXY server https://github.com/bouskaM/currency-proxy-server is used to avoid CORS issues.
- * The server is deployed on Vercel - > https://currency-proxy-server.vercel.app
+ * This code uses a proxy server to avoid CORS issues.
+ * The proxy server is hosted on Vercel at https://currency-proxy-server.vercel.app.
+ * When running locally, the server fetches data from localhost:3001.
+ * To run the server locally, follow the instructions at https://github.com/bouskaM/currency-proxy-server.
  */
 const url = "https://currency-proxy-server.vercel.app/api/daily-rates"
+const localUrl = "http://localhost:3001/api/daily-rates"
+
 export const fetchRates = async () => {
-    const requestUrl = url;
+    const requestUrl = process.env.NODE_ENV === 'development' ? localUrl : url;
     const response = await fetch(requestUrl)
     const text = await response.text();
     return text;
